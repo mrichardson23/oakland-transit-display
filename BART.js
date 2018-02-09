937c10ea486c423e4fefecbd600a20a01fde3e38
Matt Richardson <mrichardson23@gmail.com>
2018-02-09 08:47:43 -0800

    initial commit

---
+++ BART.js
@@ -0,0 +1,51 @@
+function get_bart_etds(){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET","https://api.bart.gov/api/etd.aspx?cmd=etd&orig=12TH&key=MW9S-E7SL-26DU-VV8V&json=y",false);
+    Httpreq.send(null);
+    var etd_json = JSON.parse(Httpreq.responseText);
+    etd_string_minutes = etd_json.root.station[0].etd[0].estimate[0].minutes;
+    etd_string_destinations = etd_json.root.station[0].etd[0].destination;
+    full_string = "";
+
+    for (var i = 0; i < etd_json.root.station[0].etd.length; i++) {
+ 		full_string = full_string + "<p><span class=\"destination\">"
+    	full_string = full_string + etd_json.root.station[0].etd[i].destination;
+    	full_string = full_string + "</span><span class=\"minutes\">";
+    	for (var j = 0; j < etd_json.root.station[0].etd[i].estimate.length; j++) {
+            if (etd_json.root.station[0].etd[i].estimate[j].minutes != "Leaving") {
+                full_string = full_string + " " + etd_json.root.station[0].etd[i].estimate[j].minutes;
+                if (j < etd_json.root.station[0].etd[i].estimate.length - 1) {
+                    full_string = full_string + ", "
+                }
+            }
+    	}
+    	full_string = full_string + "</span>></p>";
+    }
+    document.getElementById("bart_etds").innerHTML = full_string ;
+}
+
+
+function get_ac_transit_etd() {
+    var acHttpreq = new XMLHttpRequest(); // a new request
+    acHttpreq.open("GET","http://api.actransit.org/transit/stops/50212/predictions?token=E45E1AC8A77E05B2FAD062161A657299",false);
+    acHttpreq.send(null);
+    var etd_json = JSON.parse(acHttpreq.responseText);
+    etd_ac_transit_date = new Date(etd_json[0].PredictedDeparture)
+    if (etd_ac_transit_date.getHours() > 12) {
+        hours = etd_ac_transit_date.getHours() - 12;
+        postfix = "pm";
+    }
+    else {
+        hours = etd_ac_transit_date.getHours();
+        postfix = "am";
+    }
+        
+    document.getElementById("act_etds").innerHTML = "<p><span class=\"destination\">51A to Alameda</span></span><span class=\"minutes\">" + hours + ":" + etd_ac_transit_date.getMinutes() + postfix + "</span>></p>";
+}
+
+function refresh_times(){
+    get_bart_etds();
+    get_ac_transit_etd();
+}
+
+setInterval(refresh_times, 20000);
